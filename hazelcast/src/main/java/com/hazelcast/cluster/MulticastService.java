564f2b9552e7d1f18b95365d2344e92232491146
Rico Neubauer <R.Neubauer@seeburger.de>
2014-06-05 15:42:48 +0200

    Fixes #2492 Lowering log level and including sender.

--- hazelcast/src/main/java/com/hazelcast/cluster/MulticastService.java
+++ hazelcast/src/main/java/com/hazelcast/cluster/MulticastService.java
@@ -159,8 +159,9 @@ public class MulticastService implements Runnable {
                 }
             } catch (Exception e) {
                 if (e instanceof EOFException || e instanceof HazelcastSerializationException) {
-                    logger.warning("Received data format is invalid." +
-                            " (An old version of Hazelcast may be running here.)", e);
+                    logger.info("Received data format is invalid." +
+                            " (An old version of Hazelcast may be running here.), Sender -> "
+                                    + datagramPacketReceive.getAddress() + ", Exception -> " + e);
                 } else {
                     throw e;
                 }
